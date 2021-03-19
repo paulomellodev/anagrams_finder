73345b3be894fe10f604225a8def66622a03ce0c
Paulo Mello <rapha.mello87@gmail.com>
2021-03-18 23:46:25 -0300

    Estrutura básica montada

---
+++ anagrams1.js
@@ -0,0 +1,13 @@
+function getAnagramsOf(input){}
+
+
+const button = document.getElementById("findButton");
+button.addEventListener("click", function () {
+  // seu código vai aqui
+});
+
+button.addEventListener("click", function () {
+    let typedText = document.getElementById("input").value;
+    getAnagramsOf(typedText);
+  });
+  
