fb6af749705f8b7bea228f7ce84d750cc3146797
Paulo Mello <rapha.mello87@gmail.com>
2021-03-19 16:19:40 -0300

    funções implantadas

--- anagrams1.js
+++ anagrams1.js
@@ -1,10 +1,27 @@
-function getAnagramsOf(input){}
+function getAnagramsOf(input){
 
+  let getAlphabetize = (alphabetize(input))
+  console.log(getAlphabetize)
+
+  let result = []
+
+  palavras.map(item => {
+    let wordAlpha = alphabetize(item);
+    if(wordAlpha == getAlphabetize){
+      result.push(item)
+    }
+  })
+
+  result.splice(result.indexOf(input))
+  
+  return result
+}
+
+function alphabetize(a) {
+  return a.toLowerCase().split("").sort().join("").trim();
+}
 
 const button = document.getElementById("findButton");
-button.addEventListener("click", function () {
-  // seu código vai aqui
-});
 
 button.addEventListener("click", function () {
     let typedText = document.getElementById("input").value;
