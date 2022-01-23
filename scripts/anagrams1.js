d9077bd44eae50a23194585d5b65c4e84c8bd5fc
Paulo Mello <rapha.mello87@gmail.com>
2022-01-23 17:14:06 -0300

    update: syntax of ES6

--- scripts/anagrams1.js
+++ scripts/anagrams1.js
@@ -1,113 +1,116 @@
-const menu = document.getElementById('menu')
-menu.addEventListener('click', (event) => {
-    let target = event.currentTarget.children;
-    let clickedElement = event.target.id;
-    changePage(clickedElement, target)
+const menu = document.getElementById("menu");
+menu.addEventListener("click", (event) => {
+  let target = event.currentTarget.children;
+  let clickedElement = event.target.id;
+  changePage(clickedElement, target);
+});
 
-})
+const sections = [
+  "findAnagrams",
+  "setsOfAnagrams",
+  "twoWordAnagrams",
+  "threeWordAnagrams",
+];
 
-const sections = ['findAnagrams', 'setsOfAnagrams', 'twoWordAnagrams', 'threeWordAnagrams']
 const changePage = (clicked, childTarget) => {
-    for (let element = 0; element < childTarget.length; element++){
-        const idElement = childTarget[element].id;
-        const section = sections[element]
-        const sectionToChange = document.getElementById(section);
-        const divToChange = document.getElementById(idElement);
+  for (let element = 0; element < childTarget.length; element++) {
+    const idElement = childTarget[element].id;
+    const section = sections[element];
+    const sectionToChange = document.getElementById(section);
+    const divToChange = document.getElementById(idElement);
 
-        if(idElement === clicked){
-            divToChange.classList.add('active')
-            sectionToChange.classList.remove('hidden')
-        } else {
-            divToChange.classList.remove('active')
-            sectionToChange.classList.add('hidden')
-        }
-    };
-}
-
-function alphabetize(wordToAlphabetical) {
-    return wordToAlphabetical.toLowerCase().split('').sort().join('').trim();
-} 
-
-function words(input) {
-    return input.split(' ');
-}
-
-function findAnagrams(typedWordsAlphabetical, result, db){
-    let output = result;
-
-    db.map(word => {
-        let wordAlpha = alphabetize(word);
-        for (let i = 0; i < typedWordsAlphabetical.length; i++){
-            if(wordAlpha === typedWordsAlphabetical[i]){
-                output[wordAlpha].push(word)
-            }
-        }
-    })
-    return output
-}
-
-function getAnagramsOf(input, db){
-    let result = {}
-    const wordsInAlphabetical = input.map(word => {
-        let wordAlphabetical = alphabetize(word)
-        result[wordAlphabetical] = [];
-        return wordAlphabetical
-    });
-    result = findAnagrams(wordsInAlphabetical, result, db)
-    return result
-}
-
-function createDiv(anagrams){
-    let createDiv = document.createElement('div')
-
-    let newHeader = document.createElement('h3')
-    newHeader.textContent = 'Palavras encontradas';
-  
-    createDiv.appendChild(newHeader)
- 
-    for (item in anagrams){
-        let paragraphContent = `${item} = ${anagrams[item]}`
-        let newParagraph = document.createElement('p')
-        newParagraph.textContent = paragraphContent
-        createDiv.appendChild(newParagraph)   
+    if (idElement === clicked) {
+      divToChange.classList.add("active");
+      sectionToChange.classList.remove("hidden");
+    } else {
+      divToChange.classList.remove("active");
+      sectionToChange.classList.add("hidden");
     }
-    return createDiv
-}
+  }
+};
 
-function filterWordsByLength(size, db){
-    let filteredWords = [];
-    filteredWords = db.filter(word => word.length === size)
-    return filteredWords
-}
+const alphabetize = (wordToAlphabetical) => {
+  return wordToAlphabetical.toLowerCase().split("").sort().join("").trim();
+};
 
-function longestWord(db) {
-    let maxLength = 0
-    for (let i =0; i <db.length; i++) {
-        if(db[i].length > maxLength){
-            maxLength = db[i].length;
-        }
+const words = (input) => {
+  return input.split(" ");
+};
+
+const findAnagrams = (typedWordsAlphabetical, result, db) => {
+  let output = result;
+
+  db.map((word) => {
+    let wordAlpha = alphabetize(word);
+    for (let i = 0; i < typedWordsAlphabetical.length; i++) {
+      if (wordAlpha === typedWordsAlphabetical[i]) {
+        output[wordAlpha].push(word);
+      }
     }
-    return maxLength
-}
+  });
+  return output;
+};
+
+const getAnagramsOf = (input, db) => {
+  let result = {};
+  const wordsInAlphabetical = input.map((word) => {
+    let wordAlphabetical = alphabetize(word);
+    result[wordAlphabetical] = [];
+    return wordAlphabetical;
+  });
+  result = findAnagrams(wordsInAlphabetical, result, db);
+  return result;
+};
+
+const createDiv = (anagrams) => {
+  let createDiv = document.createElement("div");
+
+  let newHeader = document.createElement("h3");
+  newHeader.textContent = "Palavras encontradas";
+
+  createDiv.appendChild(newHeader);
+
+  for (item in anagrams) {
+    let paragraphContent = `${item} = ${anagrams[item]}`;
+    let newParagraph = document.createElement("p");
+    newParagraph.textContent = paragraphContent;
+    createDiv.appendChild(newParagraph);
+  }
+  return createDiv;
+};
+
+const filterWordsByLength = (size, db) => {
+  let filteredWords = [];
+  filteredWords = db.filter((word) => word.length === size);
+  return filteredWords;
+};
+
+const longestWord = (db) => {
+  let maxLength = 0;
+  for (let i = 0; i < db.length; i++) {
+    if (db[i].length > maxLength) {
+      maxLength = db[i].length;
+    }
+  }
+  return maxLength;
+};
 
 const btnFindAnagrams = document.getElementById("btnFindAnagrams");
-btnFindAnagrams.addEventListener('click', () => {
-    
-    let resultDiv = document.getElementById("anagramsResult");
-    resultDiv.innerHTML = "";
+btnFindAnagrams.addEventListener("click", () => {
+  let resultDiv = document.getElementById("anagramsResult");
+  resultDiv.innerHTML = "";
 
-    let typedText = document.getElementById("input").value;
-    let typedWords = words(typedText);
-    let anagrams = getAnagramsOf(typedWords, palavras);
+  let typedText = document.getElementById("input").value;
+  let typedWords = words(typedText);
+  let anagrams = getAnagramsOf(typedWords, palavras);
 
-    resultDiv.appendChild(createDiv(anagrams))
+  resultDiv.appendChild(createDiv(anagrams));
 });
-  
+
 const btnSetsOfAnagrams = document.getElementById("setsOfAnagrams");
-btnSetsOfAnagrams.addEventListener('click', () => {
-    let anagrams = getSetsOfFiveAnagrams();
-    let resultDiv = document.getElementById("setAnagramsResult");
-    resultDiv.innerHTML = "";
-    resultDiv.appendChild(createDiv(anagrams))
+btnSetsOfAnagrams.addEventListener("click", () => {
+  let anagrams = getSetsOfFiveAnagrams();
+  let resultDiv = document.getElementById("setAnagramsResult");
+  resultDiv.innerHTML = "";
+  resultDiv.appendChild(createDiv(anagrams));
 });
-  
--- scripts/setsAnagrams.js
+++ scripts/setsAnagrams.js
@@ -1,175 +1,178 @@
-const maxLength = longestWord(palavras)
+const maxLength = longestWord(palavras);
 
-function getSetsOfAnagrams(db) {
-    let output = [{}];
-    
-    for(let wordsLength = 1; wordsLength <= maxLength; wordsLength++){
-        let result = {}
-        let filteredWords = filterWordsByLength(wordsLength, db)
-        
-        filteredWords.map(item => {
-            let wordAlphabetical = alphabetize(item)
-            if(result[wordAlphabetical] === undefined) {
-                result[wordAlphabetical] = [item];
-            } else {
-                result[wordAlphabetical].push(item);
-            }
-        })
-        output.push(result)
-    }
-    return output
-}
+const getSetsOfAnagrams = (db) => {
+  let output = [{}];
+
+  for (let wordsLength = 1; wordsLength <= maxLength; wordsLength++) {
+    let result = {};
+    let filteredWords = filterWordsByLength(wordsLength, db);
+
+    filteredWords.map((item) => {
+      let wordAlphabetical = alphabetize(item);
+      if (result[wordAlphabetical] === undefined) {
+        result[wordAlphabetical] = [item];
+      } else {
+        result[wordAlphabetical].push(item);
+      }
+    });
+    output.push(result);
+  }
+  return output;
+};
 
 // criar conjuntos de anagramas por tamanho de palavras
-const setsOfAnagrams = getSetsOfAnagrams(palavras)
-
-
+const setsOfAnagrams = getSetsOfAnagrams(palavras);
 
 // criar array de anagramsa com cinco ou mais palavras
 const getSetsOfFiveAnagrams = () => {
-    const setsOfFive = {};
-    for(let i = 0; i < setsOfAnagrams.length; i++) {
-        let arr = setsOfAnagrams[i]
-        for(item in arr){
-            if(arr[item].length >= 5){
-                setsOfFive[item] = [arr[item]];
-            } 
-        }
+  let counter = 0;
+  const setsOfFive = {};
+  for (let i = 0; i < setsOfAnagrams.length; i++) {
+    let arr = setsOfAnagrams[i];
+    for (item in arr) {
+      if (arr[item].length >= 5) {
+        setsOfFive[item] = [arr[item]];
+        counter++;
+      }
     }
-    return setsOfFive
-}
+  }
+  console.log(counter);
+  return setsOfFive;
+};
 
 const verifyInput = (input, key) => {
-    let splitedInput = input.split('');
-    let splitedKey = key.split('');
+  let splitedInput = input.split("");
+  let splitedKey = key.split("");
 
-    for (let index = 0; index < splitedKey.length; index++){
-        if(!splitedInput.includes(splitedKey[index])){
-            return false
-        } else {
-            let positionOfLetter = splitedInput.indexOf(splitedKey[index])
-            splitedInput.splice(positionOfLetter, 1)
-        }
+  for (let index = 0; index < splitedKey.length; index++) {
+    if (!splitedInput.includes(splitedKey[index])) {
+      return false;
+    } else {
+      let positionOfLetter = splitedInput.indexOf(splitedKey[index]);
+      splitedInput.splice(positionOfLetter, 1);
     }
-    return true
-}
-
-
+  }
+  return true;
+};
 
 //2 anagramas
 const getAnagramsOfTwoWords = (text) => {
-    let output = []
-    
-    let textLength = text.length;
-    
-    for(let i = 1; i <= textLength-i; i++){
-        // quais conjuntos serão utilizados
-        let firstSet = setsOfAnagrams[i]
-        
-        if(textLength-i <= maxLength){
-            let lastSet = setsOfAnagrams[textLength-i];
-            
-            // iterar sobre os conjuntos selecionados
-            for(item in firstSet){
-                if(verifyInput(text, item)){
-                    for(item2 in lastSet){
-                        if(verifyInput(text, item2)){
-                            let partial = alphabetize(item+item2)
-                            if(partial === text){
-                                // criar output com dados que batem
-                                output.push(`[${firstSet[item]}] + [${lastSet[item2]}]`)
-                            }
-                        }
-                    }
-                }
+  let output = [];
+
+  let textLength = text.length;
+
+  for (let i = 1; i <= textLength - i; i++) {
+    // quais conjuntos serão utilizados
+    let firstSet = setsOfAnagrams[i];
+
+    if (textLength - i <= maxLength) {
+      let lastSet = setsOfAnagrams[textLength - i];
+      // iterar sobre os conjuntos selecionados
+      for (item in firstSet) {
+        if (verifyInput(text, item)) {
+          for (item2 in lastSet) {
+            if (verifyInput(text, item2)) {
+              let partial = alphabetize(item + item2);
+              if (partial === text) {
+                // criar output com dados que batem
+                output.push(`[${firstSet[item]}] + [${lastSet[item2]}]`);
+              }
             }
+          }
         }
+      }
     }
-    return output
-}
+  }
+  return output;
+};
 
 const btnTwoWords = document.getElementById("btnTwoWordAnagrams");
-btnTwoWords.addEventListener('click', () => {
-    
-    let resultDiv = document.getElementById("twoWordAnagramsResult");
-    resultDiv.innerHTML = "";
-    
-    // pegar o texto digitado e coloca-lo em ordem
-    let typedText = document.getElementById("input2").value;
-    let alphabeticalSentence = alphabetize(typedText);
-    
-    // verificar quantas letras possui o texto digitado
-    let twoAnagrams = getAnagramsOfTwoWords(alphabeticalSentence)
-    console.log(twoAnagrams)
+btnTwoWords.addEventListener("click", () => {
+  let resultDiv = document.getElementById("twoWordAnagramsResult");
+  resultDiv.innerHTML = "";
 
-    resultDiv.appendChild(createDiv(twoAnagrams))
+  // pegar o texto digitado e coloca-lo em ordem
+  let typedText = document.getElementById("input2").value;
+  let alphabeticalSentence = alphabetize(typedText);
+
+  // verificar quantas letras possui o texto digitado
+  let twoAnagrams = getAnagramsOfTwoWords(alphabeticalSentence);
+  console.log(twoAnagrams);
+
+  resultDiv.appendChild(createDiv(twoAnagrams));
 });
-
-
 
 //3 anagramas
 const btnThreeWords = document.getElementById("btnThreeWordAnagrams");
-btnThreeWords.addEventListener('click', () => {
-    
-    let resultDiv = document.getElementById("threeWordAnagramsResult");
-    resultDiv.innerHTML = "";
-    
-    // pegar o texto digitado e coloca-lo em ordem
-    let typedText = document.getElementById("input3").value;
-    let alphabeticalSentence = alphabetize(typedText);
-    
-    // verificar quantas letras possui o texto digitado
-    let threeAnagrams = getAnagramsOfThreeWords(alphabeticalSentence)
-    console.log(threeAnagrams)
+btnThreeWords.addEventListener("click", () => {
+  let resultDiv = document.getElementById("threeWordAnagramsResult");
+  resultDiv.innerHTML = "";
 
-    resultDiv.appendChild(createDiv(threeAnagrams))
+  // pegar o texto digitado e coloca-lo em ordem
+  let typedText = document.getElementById("input3").value;
+  let alphabeticalSentence = alphabetize(typedText);
+
+  // verificar quantas letras possui o texto digitado
+  let threeAnagrams = getAnagramsOfThreeWords(alphabeticalSentence);
+  console.log(threeAnagrams);
+
+  resultDiv.appendChild(createDiv(threeAnagrams));
 });
 
 const getAnagramsOfThreeWords = (text) => {
-    let output = []
-    let possibleWords = []
-    let textLength = text.length;
-    
-    for(let i = 0; i < textLength-1; i++){
-        let set = setsOfAnagrams[i]
-        let result = {}
-        
-        //verificar se letras do DB estão contidas nas palavras digitadas
-        for(word in set){
-            if(verifyInput(text, word)) {
-                result[word] = set[word]    
-            }
-        }
-        possibleWords.push(result)
-    }
-    
-    // iterar sobre os conjuntos selecionados
-    for(let setOneIndex = 1; setOneIndex <= textLength-setOneIndex; setOneIndex++){
-        let firstSet = possibleWords[setOneIndex]
-        
-        for(let setTwoIndex = setOneIndex; setTwoIndex <= textLength-(setOneIndex+setTwoIndex); setTwoIndex++ ){
-            let secondSet = possibleWords[setTwoIndex]
-            let setLastIndex = textLength-(setOneIndex+setTwoIndex);
-            console.log(setOneIndex, setTwoIndex, setLastIndex)
-            
-            if(setLastIndex <= maxLength+1) {
+  let output = [];
+  let possibleWords = [];
+  let textLength = text.length;
 
-                let lastSet = possibleWords[setLastIndex]
-                for(word1 in firstSet){
-                    for(word2 in secondSet){
-                        let partial = alphabetize(word1 + word2)
-                        if(verifyInput(text, partial)){
-                            for(word3 in lastSet){
-                                let complete = alphabetize(partial + word3)
-                                if(complete === text){
-                                    output.push(`[${firstSet[word1]}] + [${secondSet[word2]}] + [${lastSet[word3]}]`)
-                                }                            
-                            }
-                        }
-                    }
-                }
-            }
-        }
+  for (let i = 0; i < textLength - 1; i++) {
+    let set = setsOfAnagrams[i];
+    let result = {};
+
+    //verificar se letras do DB estão contidas nas palavras digitadas
+    for (word in set) {
+      if (verifyInput(text, word)) {
+        result[word] = set[word];
+      }
     }
-    return output
-}
+    possibleWords.push(result);
+  }
+
+  // iterar sobre os conjuntos selecionados
+  for (
+    let setOneIndex = 1;
+    setOneIndex <= textLength - setOneIndex;
+    setOneIndex++
+  ) {
+    let firstSet = possibleWords[setOneIndex];
+
+    for (
+      let setTwoIndex = setOneIndex;
+      setTwoIndex <= textLength - (setOneIndex + setTwoIndex);
+      setTwoIndex++
+    ) {
+      let secondSet = possibleWords[setTwoIndex];
+      let setLastIndex = textLength - (setOneIndex + setTwoIndex);
+      console.log(setOneIndex, setTwoIndex, setLastIndex);
+
+      if (setLastIndex <= maxLength + 1) {
+        let lastSet = possibleWords[setLastIndex];
+        for (word1 in firstSet) {
+          for (word2 in secondSet) {
+            let partial = alphabetize(word1 + word2);
+            if (verifyInput(text, partial)) {
+              for (word3 in lastSet) {
+                let complete = alphabetize(partial + word3);
+                if (complete === text) {
+                  output.push(
+                    `[${firstSet[word1]}] + [${secondSet[word2]}] + [${lastSet[word3]}]`
+                  );
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+  return output;
+};
